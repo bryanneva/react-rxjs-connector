56023a7f8a15b43dcb827aa6b7355cafa362dcec
Bryan Neva <bneva@pivotal.io>
2019-12-11 15:14:57 -0500

    Update package.json to include npm repository commands

--- index.js
+++
@@ -1,7 +0,0 @@
-import {rerenderOnChange, useBehaviorSubject, useStore} from "./lib";
-
-exports = {
-    rerenderOnChange: rerenderOnChange,
-    useBehaviorSubject: useBehaviorSubject,
-    useStore: useStore
-};
